c1c0c257f5ae48b7f5726c954c6bc5f6031cc327
PrzemyslawR <51419578+PrzemyslawRodzik@users.noreply.github.com>
2020-04-24 22:51:35 +0200

    model validation

--- PasswordManagerApp/Models/LoginModel.cs
+++ PasswordManagerApp/Models/LoginModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,9 @@ namespace PasswordManagerApp.Models
 {
     public class LoginModel
     {
+        [Required]
         public string Email { get; set; }
+        [Required]
         public string Password { get; set; }
 
     }
--- PasswordManagerApp/Models/RegisterModel.cs
+++ PasswordManagerApp/Models/RegisterModel.cs
@@ -1,14 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace PasswordManagerApp.Models
 {
     public class RegisterModel
-    {
+    {   [Required]
         public string Email { get; set; }
+        [Required]
         public string Password { get; set; }
+        [Required]
         public string  RepeatPassword { get; set; }
 
     }
