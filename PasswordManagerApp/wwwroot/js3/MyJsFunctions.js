69a9ba4bee28b5977479486566e49d3470883f83
TheAlberro <Albert.Rachwal@pollub.edu.pl>
2020-11-24 17:48:11 +0100

    poprawki

---
+++ PasswordManagerApp/wwwroot/js3/MyJsFunctions.js
@@ -0,0 +1,46 @@
+﻿
+
+/*
+ *  
+ * Wylogowanie po 60 min nieaktywności
+ * 
+ */
+
+var inactivityTime = function () {
+    var time;
+    
+    window.onload = resetTimer;
+
+    // DOM Events
+    window.onmousemove = resetTimer;
+    window.onmousedown = resetTimer;  // catches touchscreen presses as well      
+    window.ontouchstart = resetTimer; // catches touchscreen swipes as well 
+    window.onclick = resetTimer;      // catches touchpad clicks as well
+    window.onkeypress = resetTimer;
+
+    function logout() {
+        
+        window.location = '/auth/logout';
+        
+        
+    }
+
+    function resetTimer() {
+        clearTimeout(time);
+        time = setTimeout(logout, 1000 * 60 * 60 )
+        // 1000 milliseconds = 1 second
+    }
+};
+
+window.onload = function () {
+    
+    
+        inactivityTime();
+    
+        
+}
+
+
+
+
+
